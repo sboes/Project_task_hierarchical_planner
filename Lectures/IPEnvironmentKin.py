aebf0dd33761f762604022e0da8394efc5755c20
sboes <simon.boes@student.kit.edu>
2025-07-01 19:40:22 +0200

    updated Lecture codes

--- Lectures/IPEnvironmentKin.py
+++ Lectures/IPEnvironmentKin.py
@@ -1,3 +1,4 @@
+
 """
 This code is part of a series of notebooks regarding  "Introduction to robot path planning".
 
@@ -11,7 +12,6 @@ from shapely.geometry import Point, Polygon, LineString
 from shapely import plotting
 import numpy as np
 import copy
-
 
 def interpolate_line(startPos, endPos, step_l):
     steps = []
@@ -32,23 +32,22 @@ class KinChainCollisionChecker(CollisionChecker):
     def __init__(self, kin_chain, scene, limits=[[-3.0, 3.0], [-3.0, 3.0]], statistic=None, fk_resolution=0.1):
         super(KinChainCollisionChecker, self).__init__(scene, limits, statistic)
         if len(limits) != kin_chain.dim:
-            raise ValueError(
-                "Limits must match the dimension of the kinematic chain. Default values are for a 2-dof planar manipulator. If you use dof>2 you have to specify the limits explicitly")
+            raise ValueError("Limits must match the dimension of the kinematic chain. Default values are for a 2-dof planar manipulator. If you use dof>2 you have to specify the limits explicitly")
         self.kin_chain = kin_chain
         self.fk_resolution = fk_resolution
         self.dim = self.kin_chain.dim
 
     def getDim(self):
         return self.dim
-
+    
     def pointInCollision(self, pos):
         self.kin_chain.move(pos)
         joint_positions = self.kin_chain.get_transforms()
         for i in range(1, len(joint_positions)):
-            if self.segmentInCollision(joint_positions[i - 1], joint_positions[i]):
+            if self.segmentInCollision(joint_positions[i-1], joint_positions[i]):
                 return True
         return False
-
+    
     def lineInCollision(self, startPos, endPos):
         assert (len(startPos) == self.getDim())
         assert (len(endPos) == self.getDim())
@@ -57,42 +56,41 @@ class KinChainCollisionChecker(CollisionChecker):
             if self.pointInCollision(pos):
                 return True
         return False
-
+    
     def segmentInCollision(self, startPos, endPos):
         for key, value in self.scene.items():
             if value.intersects(LineString([(startPos[0], startPos[1]), (endPos[0], endPos[1])])):
                 return True
         return False
-
+    
     def drawObstacles(self, ax, inWorkspace=False):
         if inWorkspace:
             for key, value in self.scene.items():
                 plotting.plot_polygon(value, add_points=False, color='red', ax=ax)
 
 
+
 def planarRobotVisualize(kin_chain, ax):
     joint_positions = kin_chain.get_transforms()
     for i in range(1, len(joint_positions)):
-        xs = [joint_positions[i - 1][0], joint_positions[i][0]]
-        ys = [joint_positions[i - 1][1], joint_positions[i][1]]
+        xs = [joint_positions[i-1][0], joint_positions[i][0]]
+        ys = [joint_positions[i-1][1], joint_positions[i][1]]
         ax.plot(xs, ys, color='g')
-
-
-import matplotlib.pyplot as plt
+        
+  
+import matplotlib.pyplot as plt              
 import matplotlib.animation
 from IPython.display import HTML
 
 matplotlib.rcParams['animation.embed_limit'] = 64
-
-
-def animateSolution(planner, environment, solution, visualizer, workSpaceLimits=[[-3, 3], [-3, 3]]):
+def animateSolution(planner, environment, solution, visualizer, workSpaceLimits=[[-3,3],[-3,3]]):
     _planner = planner
     _environment = environment
     _solution = solution
     _prmVisualizer = visualizer
-
+    
     if _environment.getDim() == 2:
-
+    
         fig_local = plt.figure(figsize=(14, 7))
         ax1 = fig_local.add_subplot(1, 2, 1)
         ax2 = fig_local.add_subplot(1, 2, 2)
@@ -101,14 +99,14 @@ def animateSolution(planner, environment, solution, visualizer, workSpaceLimits=
         ## interpolate to obtain a smoother movement
         i_solution_pos = [solution_pos[0]]
         for i in range(1, len(solution_pos)):
-            segment_s = solution_pos[i - 1]
+            segment_s = solution_pos[i-1]
             segment_e = solution_pos[i]
             i_solution_pos = i_solution_pos + interpolate_line(segment_s, segment_e, 0.1)[1:]
         ## animate
         frames = len(i_solution_pos)
-
+        
         r = environment.kin_chain
-
+        
         def animate(t):
             ## clear taks space figure
             ax1.cla()
@@ -116,12 +114,12 @@ def animateSolution(planner, environment, solution, visualizer, workSpaceLimits=
             ax1.set_xlim(workSpaceLimits[0])
             ax1.set_ylim(workSpaceLimits[1])
             ## draw obstacles
-            _environment.drawObstacles(ax1, inWorkspace=True)
+            _environment.drawObstacles(ax1, inWorkspace = True)
             ## update robot position
             pos = i_solution_pos[t]
             r.move(pos)
             planarRobotVisualize(r, ax1)
-
+        
             ## clear joint space figure
             ax2.cla()
             ## draw graph and path
@@ -141,14 +139,14 @@ def animateSolution(planner, environment, solution, visualizer, workSpaceLimits=
         ## interpolate to obtain a smoother movement
         i_solution_pos = [solution_pos[0]]
         for i in range(1, len(solution_pos)):
-            segment_s = solution_pos[i - 1]
+            segment_s = solution_pos[i-1]
             segment_e = solution_pos[i]
             i_solution_pos = i_solution_pos + interpolate_line(segment_s, segment_e, 0.1)[1:]
         ## animate
         frames = len(i_solution_pos)
-
+        
         r = environment.kin_chain
-
+        
         def animate(t):
             ## clear taks space figure
             ax1.cla()
@@ -156,12 +154,13 @@ def animateSolution(planner, environment, solution, visualizer, workSpaceLimits=
             ax1.set_xlim(workSpaceLimits[0])
             ax1.set_ylim(workSpaceLimits[1])
             ## draw obstacles
-            _environment.drawObstacles(ax1, inWorkspace=True)
+            _environment.drawObstacles(ax1, inWorkspace = True)
             ## update robot position
             pos = i_solution_pos[t]
             r.move(pos)
             planarRobotVisualize(r, ax1)
-
+        
+        
         ani = matplotlib.animation.FuncAnimation(fig_local, animate, frames=frames)
         html = HTML(ani.to_jshtml())
         display(html)
